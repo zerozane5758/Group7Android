6ea504e6253bcc906581adeb08c145f441a77aaf
richarddstevenn <131933833+richarddstevenn@users.noreply.github.com>
2024-12-23 12:56:58 +0700

    penambahan hal tiket

--- app/src/main/java/g7/project/group7android/TicketActivity.kt
+++ app/src/main/java/g7/project/group7android/TicketActivity.kt
@@ -5,15 +5,14 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OrderActivity : AppCompatActivity() {
+class TicketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_order)
+        setContentView(R.layout.activity_ticket)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -26,16 +25,5 @@ class OrderActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val _price1 = findViewById<ConstraintLayout>(R.id.price1)
-        val _price2 = findViewById<ConstraintLayout>(R.id.price2)
-        val _price3 = findViewById<ConstraintLayout>(R.id.price3)
-        val _price4 = findViewById<ConstraintLayout>(R.id.price4)
-        val _price5 = findViewById<ConstraintLayout>(R.id.price5)
-
-        _price1.setOnClickListener {
-            val intent = Intent(this, TicketActivity::class.java)
-            startActivity(intent)
-
-        }
     }
 }
